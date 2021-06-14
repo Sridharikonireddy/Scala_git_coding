b9b5b5e91b671f83d3cc334664f26378a1a09bfa
Sridharikonireddy <85837965+Sridharikonireddy@users.noreply.github.com>
2021-06-14 19:00:05 +0530

    Create Variables.scala

---
+++ Variables.scala
@@ -0,0 +1,21 @@
+# Scala_git_coding
+/*object variables
+{
+  var a=12
+   def main(args:Array[String]): Unit =
+  {
+    var a="Sring
+    var a1='@'
+    println(a)
+  }
+}*/
+object variables {
+  val a = 12
+
+  def main(args: Array[String]): Unit = {
+    val a = "Sring"
+    var a1 = '@'
+    var data:String = "sree"
+    println(data)
+  }
+}
